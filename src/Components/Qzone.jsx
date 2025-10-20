2f23830b8d53d95a2ec7808da8a026d76f1b7952
Hasib Shrabon <hasibahmedshrabon@gmail.com>
2025-10-20 17:05:13 +0300

    home page last commit

---
+++ src/Components/Qzone.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Qzone = () => {
+  return (
+    <div className="grid grid-cols-1 gap-5">
+      <div>
+        <img src="/swimming.png" alt="" />
+      </div>
+      <div>
+        <img src="/playground.png" alt="" />
+      </div>
+      <div>
+        <img src="/class.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Qzone;
